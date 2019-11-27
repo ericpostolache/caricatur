2e640019f7aa4bb4a441e76c64c9e849e7e17d32
Stefanescu Andreea <andreea.stefanescu21@gmail.com>
2019-11-27 13:21:51 +0200

    Added import from gallery button

--- app/src/main/java/com/example/caricatur/MainActivity.java
+++ app/src/main/java/com/example/caricatur/MainActivity.java
@@ -9,15 +9,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.Manifest;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -25,16 +31,32 @@ import androidx.core.content.FileProvider;
 
 
 
-public class MainActivity<Button> extends Activity {
+public class MainActivity extends Activity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static int REQUEST_TAKE_PHOTO = 1;
+    static int GALLERY_REQUEST_CODE = 1;
+    static int INTENT_TYPE = 0;
+    private ImageView imageView;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && INTENT_TYPE == 1) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imageView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        }  else if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
+        } else {
+            Toast.makeText(this, "You haven't picked an Image",Toast.LENGTH_LONG).show();
         }
     }
 
@@ -55,9 +77,6 @@ public class MainActivity<Button> extends Activity {
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
-
-
-    static final int REQUEST_TAKE_PHOTO = 1;
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -80,21 +99,23 @@ public class MainActivity<Button> extends Activity {
             }
         }
     }
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }
+//
+//    private void galleryAddPic() {
+//        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+//        File f = new File(currentPhotoPath);
+//        Uri contentUri = Uri.fromFile(f);
+//        mediaScanIntent.setData(contentUri);
+//        this.sendBroadcast(mediaScanIntent);
+//    }
 
 
 
 
 
     private final ThreadLocal<Button> takePictureButton = new ThreadLocal<Button>();
-    private ImageView imageView;
+
+    private Button importFromGalleryButton;
+    private Button takePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,7 +130,24 @@ public class MainActivity<Button> extends Activity {
             ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
         }*/
 
-        dispatchTakePictureIntent();
+        imageView = (ImageView) findViewById(R.id.imageview);
+        importFromGalleryButton = (Button) findViewById(R.id.button_image_from_gallery);
+        takePicture = (Button) findViewById(R.id.button_image);
+        takePicture.setOnClickListener(click -> dispatchTakePictureIntent());
+        importFromGalleryButton.setOnClickListener(click ->  {
+            getPicFromGallery();
+        });
         //galleryAddPic();
+
+
+        //galleryAddPic();
+    }
+
+    private void getPicFromGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        INTENT_TYPE = 1;
+        photoPickerIntent.setType("image/*");
+        photoPickerIntent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(photoPickerIntent, GALLERY_REQUEST_CODE);
     }
 }
